96f9a29b91f7083d36238b77bcafca2fd8724cc9
Khushi Premal Desai <khushipdesai@berkeley.edu>
2020-07-30 13:40:27 -0700

    Updated amplify configurations

--- src/Login.js
+++ src/Login.js
@@ -1,1 +1,13 @@
 import React from 'react';
+import './App.css';
+import { Auth } from 'aws-amplify';
+
+const Login = (props) => {
+    return (
+        <div>
+            <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
+        </div>
+    )
+}
+
+export default Login;
